dd9124ae99120e620274e83dd5a6ea5e393e3776
Vladislav Belov <vladislavbelovdev@gmail.com>
2026-04-25 11:55:12 +0200

    Fixes Vulkan uniform buffer size after 9e371824c2
    32 was accidentally added during debugging.

--- source/renderer/backend/vulkan/DeviceCommandContext.cpp
+++ source/renderer/backend/vulkan/DeviceCommandContext.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2025 Wildfire Games.
+/* Copyright (C) 2026 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -63,7 +63,7 @@ namespace Vulkan
 namespace
 {
 
-constexpr uint32_t UNIFORM_BUFFER_INITIAL_SIZE = 1024 * 1024 * 32;
+constexpr uint32_t UNIFORM_BUFFER_INITIAL_SIZE = 1024 * 1024;
 constexpr uint32_t FRAME_INPLACE_BUFFER_INITIAL_SIZE = 128 * 1024;
 
 struct SBaseImageState
