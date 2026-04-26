AOSC-Tracking/0ad

Remove Engine.SwitchGuiPage from gamesetup

binaries/data/mods/public/gui/gamesetup/Pages/GameSetupPage/GameSetupPage.js
@@ -3,7 +3,7 @@
  */
 SetupWindowPages.GameSetupPage = class
 {
-	constructor(setupWindow, isSavedGame)
+	constructor(setupWindow, isSavedGame, cancelClicked)
 	{
 		Engine.ProfileStart("GameSetupPage");
 
@@ -18,7 +18,7 @@ SetupWindowPages.GameSetupPage = class
 				"civInfoButton": new CivInfoButton(),
 				"lobbyButton": new LobbyButton(),
 				"savedGameLabel": new SavedGameLabel(isSavedGame),
-				"cancelButton": new CancelButton(setupWindow, startGameButton, readyButton),
+				"cancelButton": new CancelButton(startGameButton, readyButton, cancelClicked),
 				"readyButton": readyButton,
 				"startGameButton": startGameButton
 			};

binaries/data/mods/public/gui/gamesetup/Pages/GameSetupPage/Panels/Buttons/CancelButton.js
@@ -1,15 +1,13 @@
 class CancelButton
 {
-	constructor(setupWindow, startGameButton, readyButton)
+	constructor(startGameButton, readyButton, cancelClicked)
 	{
-		this.setupWindow = setupWindow;
-
 		this.buttonPositions = Engine.GetGUIObjectByName("bottomRightPanel").children;
 
 		this.cancelButton = Engine.GetGUIObjectByName("cancelButton");
 		this.cancelButton.caption = this.Caption;
 		this.cancelButton.tooltip = Engine.HasXmppClient() ? this.TooltipLobby : this.TooltipMenu;
-		this.cancelButton.onPress = setupWindow.closePage.bind(setupWindow);
+		this.cancelButton.onPress = cancelClicked;
 	}
 }
 

YavorLazarovTUES/0ad-GL2026

binaries/data/mods/public/gui/gamesetup/Pages/GameSetupPage/GameSetupPage.js
@@ -3,7 +3,7 @@
  */
 SetupWindowPages.GameSetupPage = class
 {
-	constructor(setupWindow, isSavedGame)
+	constructor(setupWindow, isSavedGame, cancelCallback)
 	{
 		Engine.ProfileStart("GameSetupPage");
 
@@ -18,7 +18,7 @@ SetupWindowPages.GameSetupPage = class
 				"civInfoButton": new CivInfoButton(),
 				"lobbyButton": new LobbyButton(),
 				"savedGameLabel": new SavedGameLabel(isSavedGame),
-				"cancelButton": new CancelButton(setupWindow, startGameButton, readyButton),
+				"cancelButton": new CancelButton(startGameButton, readyButton, cancelCallback),
 				"readyButton": readyButton,
 				"startGameButton": startGameButton
 			};

binaries/data/mods/public/gui/gamesetup/Pages/GameSetupPage/Panels/Buttons/CancelButton.js
@@ -1,15 +1,13 @@
 class CancelButton
 {
-	constructor(setupWindow, startGameButton, readyButton)
+	constructor(startGameButton, readyButton, cancelCallback)
 	{
-		this.setupWindow = setupWindow;
-
 		this.buttonPositions = Engine.GetGUIObjectByName("bottomRightPanel").children;
 
 		this.cancelButton = Engine.GetGUIObjectByName("cancelButton");
 		this.cancelButton.caption = this.Caption;
 		this.cancelButton.tooltip = Engine.HasXmppClient() ? this.TooltipLobby : this.TooltipMenu;
-		this.cancelButton.onPress = setupWindow.closePage.bind(setupWindow);
+		this.cancelButton.onPress = cancelCallback;
 	}
 }
 