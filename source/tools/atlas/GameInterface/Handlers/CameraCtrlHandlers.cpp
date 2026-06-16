YavorLazarovTUES/0ad-GL2026

commit 9388692a4717f0ecccfb2e37aedddb779042dae2

Add bird view to Atlas
Fixes: #2657
Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

source/tools/atlas/GameInterface/Handlers/CameraCtrlHandlers.cpp
@@ -258,4 +258,42 @@ MESSAGEHANDLER(SetView)
 	// TODO: Rotation
 }
 
+MESSAGEHANDLER(ToggleBirdsEyeView)
+{
+	if (!g_Game || g_Game->GetView()->GetCinema()->IsPlaying())
+		return;
+
+	static bool birdEyeView{false};
+	static float declination{0.f};
+
+	CCamera& camera = AtlasView::GetView_Game()->GetCamera();
+	CMatrix3D& orientation = camera.GetOrientation();
+	CVector3D focus = camera.GetFocus();
+
+	if (!birdEyeView)
+	{
+		CVector3D in = orientation.GetIn();
+		declination = atan2(sqrt(in.X*in.X + in.Z*in.Z), in.Y) - std::numbers::pi_v<float> / 2.f;
+	}
+
+	CQuaternion q;
+	// If really 90° then the camera movement bugs out as it has no clear
+	// forward anymore, so stick with 89°.
+	q.FromAxisAngle(orientation.GetLeft(), birdEyeView ?
+		DEGTORAD(89.f) - declination : DEGTORAD(-89.f) + declination);
+
+	CVector3D origin = orientation.GetTranslation();
+	CVector3D offset = q.Rotate(origin - focus);
+
+	q *= orientation.GetRotation();
+	q.Normalize();
+	q.ToMatrix(orientation);
+
+	orientation.Translate(focus + offset);
+
+	camera.UpdateFrustum();
+
+	birdEyeView = !birdEyeView;
+}
+
 }

source/tools/atlas/AtlasUI/ScenarioEditor/ScenarioEditor.cpp
@@ -173,6 +173,12 @@ private:
 		if (KeyScroll(evt, true))
 			return;
 
+		if (evt.GetKeyCode() == 'B')
+		{
+			POST_MESSAGE(ToggleBirdsEyeView, ());
+			return;
+		}
+
 		POST_MESSAGE(GuiKeyEvent, (GetSDLKeyFromWxKeyCode(evt.GetKeyCode()), evt.GetUnicodeKey(), true));
 
 		evt.Skip();

source/tools/atlas/GameInterface/Messages.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2025 Wildfire Games.
+/* Copyright (C) 2026 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -468,6 +468,8 @@ QUERY(GetView,
 MESSAGE(SetView,
 		((sCameraInfo, info))
 		);
+
+MESSAGE(ToggleBirdsEyeView, );
 
 //////////////////////////////////////////////////////////////////////////
 

commit fab4a132e12126d32845e0b9f49fd47e37eaff4f

Fix camera reset under bird view in Atlas
Resetting camera while in bird view should reset birdview flag.

Fixes: 9388692a4717f0ecccfb2e37aedddb779042dae2
Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

source/tools/atlas/GameInterface/Handlers/CameraCtrlHandlers.cpp
@@ -37,6 +37,11 @@
 #include "tools/atlas/GameInterface/SharedTypes.h"
 #include "tools/atlas/GameInterface/View.h"
 
+namespace
+{
+bool g_BirdEyeView{false};
+} // namespace
+
 namespace AtlasMessage {
 
 MESSAGEHANDLER(CameraReset)
@@ -60,6 +65,8 @@ MESSAGEHANDLER(CameraReset)
 	}
 
 	g_Game->GetView()->ResetCameraTarget(target);
+
+	g_BirdEyeView = false;
 }
 
 MESSAGEHANDLER(ScrollConstant)
@@ -263,14 +270,13 @@ MESSAGEHANDLER(ToggleBirdsEyeView)
 	if (!g_Game || g_Game->GetView()->GetCinema()->IsPlaying())
 		return;
 
-	static bool birdEyeView{false};
 	static float declination{0.f};
 
 	CCamera& camera = AtlasView::GetView_Game()->GetCamera();
 	CMatrix3D& orientation = camera.GetOrientation();
 	CVector3D focus = camera.GetFocus();
 
-	if (!birdEyeView)
+	if (!g_BirdEyeView)
 	{
 		CVector3D in = orientation.GetIn();
 		declination = atan2(sqrt(in.X*in.X + in.Z*in.Z), in.Y) - std::numbers::pi_v<float> / 2.f;
@@ -279,7 +285,7 @@ MESSAGEHANDLER(ToggleBirdsEyeView)
 	CQuaternion q;
 	// If really 90° then the camera movement bugs out as it has no clear
 	// forward anymore, so stick with 89°.
-	q.FromAxisAngle(orientation.GetLeft(), birdEyeView ?
+	q.FromAxisAngle(orientation.GetLeft(), g_BirdEyeView ?
 		DEGTORAD(89.f) - declination : DEGTORAD(-89.f) + declination);
 
 	CVector3D origin = orientation.GetTranslation();
@@ -293,7 +299,7 @@ MESSAGEHANDLER(ToggleBirdsEyeView)
 
 	camera.UpdateFrustum();
 
-	birdEyeView = !birdEyeView;
+	g_BirdEyeView = !g_BirdEyeView;
 }
 
 }